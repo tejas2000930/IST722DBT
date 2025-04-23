2e51d559d4136c1b71ed5a6cb1efba31b34b8584
Tejas Mistry <tamistry@g.syr.edu>
2025-04-23 09:26:17 +0000

    rad

--- quickstart/models/customer_orders.sql
+++ quickstart/models/customer_orders.sql
@@ -1,9 +1,8 @@
 with customers as (
-    select
-        id           as customer_id,
-        first_name,
-        last_name
-    from raw.jaffle_shop.customers
+  select id           as customer_id,
+         first_name,
+         last_name
+    from {{ source('jaffle_shop','customers') }}
 ),
 
 orders as (
