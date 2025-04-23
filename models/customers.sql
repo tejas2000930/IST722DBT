25af17b2f76740b54164bdadd42b42bb4ab118bd
Tejas Mistry <tamistry@g.syr.edu>
2025-04-23 09:49:53 +0000

    k

--- models/customers.sql
+++ models/customers.sql
@@ -1,55 +1,29 @@
+-- quickstart/models/customers.sql
 with customers as (
-
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from {{ source('jaffle_shop','customers') }}
-
+  select * from {{ ref('stg_customers') }}
 ),
-
 orders as (
-
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from raw.jaffle_shop.orders
-
+  select * from {{ ref('stg_orders') }}
 ),
-
 customer_orders as (
-
-    select
-        customer_id,
-
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
-
-    from orders
-
-    group by 1
-
+  select
+    customer_id,
+    min(order_date) as first_order_date,
+    max(order_date) as most_recent_order_date,
+    count(order_id)   as number_of_orders
+  from orders
+  group by customer_id
 ),
-
 final as (
-
-    select
-        customers.customer_id,
-        customers.first_name,
-        customers.last_name,
-        customer_orders.first_order_date,
-        customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
-
-    from customers
-
-    left join customer_orders using (customer_id)
-
+  select
+    c.customer_id,
+    c.first_name,
+    c.last_name,
+    coalesce(co.number_of_orders,0) as number_of_orders,
+    co.first_order_date,
+    co.most_recent_order_date
+  from customers c
+  left join customer_orders co using (customer_id)
 )
 
 select * from final
